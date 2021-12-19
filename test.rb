014a7b1b2c9bc278047961f0d7e2f1f928d61e08
caegon <caegon2005@hotmail.com>
2021-12-19 20:44:42 -0300

    first

--- test.rb
+++ test.rb
@@ -12,6 +12,8 @@ def app
     Sinatra::Application
 end
 
+#elaborar uma função que cria arquivo texto com diferentes palestras com periodos aleatorios 
+
 def test_it_post_ok
     post "/upload_task", "file" => Rack::Test::UploadedFile.new("proposals.txt", "text/plain")
     assert_equal 200, last_response.status
@@ -30,6 +32,39 @@ def test_it_post_ok
     while cont < 24 do
         assert_not_include last_response.body,cont.to_s+":"
         cont += 1
+    end
+
+    #verifica horarios errados após o almoço
+    cont = 1
+    while cont < 60 do
+        if cont < 10
+            assert_not_include last_response.body,"12:0"+cont.to_s
+        else
+            assert_not_include last_response.body,"12:"+cont.to_s
+        end
+        cont += 1
+    end
+
+    #verifica horarios errados antes das 9:00
+    cont_a = 1
+    cont_b = 9
+    while cont_b < 9 do
+        while cont_a < 60 do
+            assert_not_include last_response.body,cont_b.to_s+":"+cont.to_s
+            cont_a += 1
+        end
+        cont_b += 1
+    end
+    
+    #verifica horaios errados após as 17:00
+    cont_a = 1
+    cont_b = 17
+    while cont_b < 24 do
+        while cont_a < 60 do
+            assert_not_include last_response.body,cont_b.to_s+":"+cont.to_s
+            cont_a += 1
+        end
+        cont_b += 1
     end
     
 end
