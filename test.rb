7c0506208c3811bcb5c55fea415890f34f8c21e7
caegon <caegon2005@hotmail.com>
2021-12-20 11:59:25 -0300

    update

--- test.rb
+++ test.rb
@@ -6,69 +6,59 @@ require 'rack/test'
 ENV['ENV_ENV'] = 'test'
 
 class IndexTest < Test::Unit::TestCase
-include Rack::Test::Methods
+    include Rack::Test::Methods
 
-def app
-    Sinatra::Application
-end
-
-#elaborar uma função que cria arquivo texto com diferentes palestras com periodos aleatorios 
-
-def test_it_post_ok
-    post "/upload_task", "file" => Rack::Test::UploadedFile.new("proposals.txt", "text/plain")
-    assert_equal 200, last_response.status
-    assert_include last_response.body,"9:00"
-    assert_include last_response.body,"12:00"
-    assert_include last_response.body,"17:00"
-    assert_not_include last_response.body,"00:"
-
-    cont = 1
-    while cont < 9 do
-        assert_not_include last_response.body,"0"+cont.to_s+":"
-        cont += 1
+    def app
+        Sinatra::Application
     end
 
-    cont = 18
-    while cont < 24 do
-        assert_not_include last_response.body,cont.to_s+":"
-        cont += 1
-    end
-
-    #verifica horarios errados após o almoço
-    cont = 1
-    while cont < 60 do
-        if cont < 10
-            assert_not_include last_response.body,"12:0"+cont.to_s
-        else
-            assert_not_include last_response.body,"12:"+cont.to_s
+    #elaborar uma função que cria arquivo texto com diferentes palestras com periodos aleatorios 
+    def generate_new_file
+        
+        open('proposals.txt', 'w') do |f|
+            for i in 1..(rand 1..150) 
+                task_name = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
+                min = rand 1..60
+                line = task_name + ' '+min.to_s+'min'
+                f.puts line
+            end 
         end
-        cont += 1
+
     end
 
-    #verifica horarios errados antes das 9:00
-    cont_a = 1
-    cont_b = 9
-    while cont_b < 9 do
-        while cont_a < 60 do
-            assert_not_include last_response.body,cont_b.to_s+":"+cont.to_s
-            cont_a += 1
+    def test_it_ok
+        generate_new_file
+        post "/upload_task", "file" => Rack::Test::UploadedFile.new("proposals.txt", "text/plain")
+        assert_equal 200, last_response.status
+        
+        assert_include last_response.body,"TRACK A"
+        assert_include last_response.body,"9:00"
+        assert_include last_response.body,"12:00"
+        assert_include last_response.body,"almoço"
+        assert_include last_response.body,"networking"
+
+        for j in 1..59
+            hora = '12:0'+j.to_s if j < 10
+            hora = '12:'+j.to_s if j >= 10
+            assert_not_include last_response.body,hora
         end
-        cont_b += 1
-    end
-    
-    #verifica horaios errados após as 17:00
-    cont_a = 1
-    cont_b = 17
-    while cont_b < 24 do
-        while cont_a < 60 do
-            assert_not_include last_response.body,cont_b.to_s+":"+cont.to_s
-            cont_a += 1
+
+        for i in 0..8
+            for j in 0..59
+                hora = '0'+i.to_s+':0'+j.to_s if j < 10
+                hora = '0'+i.to_s+':'+j.to_s if j >= 10
+                assert_not_include last_response.body,hora
+            end
         end
-        cont_b += 1
+
+        for i in 17..23
+            for j in 0..59
+                hora = '0'+i.to_s+':0'+j.to_s if j < 10
+                hora = '0'+i.to_s+':'+j.to_s if j >= 10
+                assert_not_include last_response.body,hora
+            end
+        end
+        
     end
-    
-end
-
-
 
 end
